05010bc9578602fadd91aaaf7b8e0b693c26f884
walterdecastro <walterdc1995@gmail.com>
2023-10-26 23:20:15 -0300

    modify settings and base.html

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -122,6 +122,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = "/media/"
+#MEDIA_URL = "/media/"
 
-MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+#MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
