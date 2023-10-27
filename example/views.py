dee24aaee90c58a95dc53fed007685cf93dc3705
walterdecastro <walterdc1995@gmail.com>
2023-10-26 23:02:12 -0300

    add bootstrap template e modify settings

--- example/views.py
+++ example/views.py
@@ -1,16 +1,5 @@
 # example/views.py
-from datetime import datetime
-
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Escola de Robôs</h1>
-            <p>Atualizando o frontend...</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'index.html')
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'vercel_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
